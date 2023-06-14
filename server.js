ee1a75edd9dd5290dd18e6f36217c129a6e0a907
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-06-14 18:34:30 +0200

    Add handlebars engine

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
+
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 const app = express();
 
@@ -30,6 +34,10 @@ app.get('/info', (req, res) => {
 
 app.get('/history', (req, res) => {
   res.show('history.html');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.send(`Hello ${req.params.name}`);
 });
 
 app.use((req, res) => {
